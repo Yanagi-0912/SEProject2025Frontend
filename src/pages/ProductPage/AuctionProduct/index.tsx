c042f72ac3f3226db1bf8e488da08ec6f4f9b3be
Yanagi-0912 <930912jeffery@gmail.com>
2025-11-02 13:28:49 +0800

    btn-to-cart

--- src/pages/ProductPage/AuctionProduct/index.tsx
+++ src/pages/ProductPage/AuctionProduct/index.tsx
@@ -7,6 +7,7 @@ interface AuctionProps {
     productPrice?: number;
     productImage?: string;
     productStock?: number;
+  productID?: string;
     productStatus?: ProductStatuses | string;
     averageRating?: number;
     auctionEndTime?: string;
@@ -22,15 +23,26 @@ function AuctionProduct(props: AuctionProps) {
     return (
       <div className="product-card">
         <div><img src={props.productImage} alt={props.productName} /></div>
-        <div className="product-title">產品名稱: {props.productName}</div>
-        <div>產品描述: {props.productDescription}</div>
-        <div>產品價格: ${props.productPrice}</div>
-        <div>庫存數量: {props.productStock}</div>
-        <div>目前狀態: {props.productStatus}</div>
-        <div>平均評分: {props.averageRating}</div>
-        <div>拍賣結束時間: {props.auctionEndTime}</div>
-        <div>目前最高出價: ${props.nowHighestBid}</div>
-        <div>最高出價者ID: {props.highestBidderID}</div>
+        <div className='product-header'>
+          <div className="product-title"> {props.productName}</div>
+          <div className="product-rating">平均評分: {props.averageRating}</div>
+        </div>
+        <div className='product-status'>
+          <div>拍賣結束時間: {props.auctionEndTime}</div>
+          <div>目前狀態: {props.productStatus === 'ACTIVE' ? '上架中' : props.productStatus === 'INACTIVE' ? '下架中' : props.productStatus === 'SOLD' ? '已售出' : '已禁用'}</div>
+        </div>
+        <div className='product-price'>
+          <div>直購價格: ${props.productPrice}</div>
+          <div>目前最高出價: ${props.nowHighestBid}</div>
+        </div>
+        {props.productStatus === 'ACTIVE' ? (
+              <div className='actionPanel'>
+                  <input type='text' placeholder='輸入出價金額' className='bidInput'/>
+                  <button className="bid-button">加入競標</button>
+              </div>
+        ) : (
+            <div className='warning-word'>*商品不可競標</div>
+        )}
       </div>
     );
 }
