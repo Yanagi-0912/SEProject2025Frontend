cc2d29f6be20660e5e768302b6a2893e1e324330
Yanagi-0912 <930912jeffery@gmail.com>
2025-11-09 17:17:40 +0800

    fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { liquidEtherConfig } from './components/backgrounds/LiquidEther/config'
 import './components/backgrounds/LiquidEther/LiquidEther.css'
 import './App.css'
 import Login from './pages/Login'
-import Main from './pages/ProductPage'
+import Main from './pages/Main'
 
 function App() {
   const [isGuest, setIsGuest] = useState(false)
