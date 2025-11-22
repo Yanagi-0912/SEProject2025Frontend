81c549716193180d9112f1b3855b5d0190cb36a2
Yanagi-0912 <930912jeffery@gmail.com>
2025-11-22 19:50:44 +0800

    更新使用說明README.md

--- orval.config.ts
+++ orval.config.ts
@@ -2,7 +2,7 @@ export default {
   auctionSystem: {
     input: 'http://localhost:8080/v3/api-docs',
     output: {
-      target: './src/api/generated.tsx',
+      target: './src/api/generated.ts',
       client: 'react-query',
       clean: true,
     },
