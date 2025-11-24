5d68f23faaed4ab86fb012e936c126407300882b
Yanagi0912 <930912jeffery@gmail.com>
2025-11-24 13:53:16 +0800

    串好後端api

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import axios from 'axios'
 import App from './App.tsx'
+
+// 設定 axios baseURL
+axios.defaults.baseURL = 'http://localhost:8080'
 
 const queryClient = new QueryClient()
 
